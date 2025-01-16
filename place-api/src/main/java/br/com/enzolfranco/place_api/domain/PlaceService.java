0d448a5cd56dab4e99f6a2a833662c703b2566e9
EnzoLFranco <fenzolemos@gmail.com>
2025-01-16 14:44:52 -0300

    fix: Modifica o PlaceService para receber o PlaceRequest como parâmetro

--- place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
@@ -1,5 +1,6 @@
 package br.com.enzolfranco.place_api.domain;
 
+import br.com.enzolfranco.place_api.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
@@ -9,7 +10,9 @@ public class PlaceService {
         this.placeRepository = placeRepository;
     }
 
-    public Mono<Place> create(Place place){
+    public Mono<Place> create(PlaceRequest placeRequest){
+        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+                placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
 }
