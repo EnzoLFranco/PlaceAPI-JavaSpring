999c3eb398f9969b915dea7ce7299a296b566d35
EnzoLFranco <fenzolemos@gmail.com>
2025-01-16 13:53:52 -0300

    feat: Adiciona a injeção do repositório PlaceRepository e seu construtor

--- place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
@@ -1,5 +1,11 @@
 package br.com.enzolfranco.place_api.domain;
 
 public class PlaceService {
+    private PlaceRepository placeRepository;
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
 
 }
