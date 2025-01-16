731ab59e83ef6c9ef7da42375a9991ca3c2d7687
EnzoLFranco <fenzolemos@gmail.com>
2025-01-16 14:17:56 -0300

    feat: Adiciona a configuração para instanciar PlaceService com PlaceRepository

--- place-api/src/main/java/br/com/enzolfranco/place_api/config/PlaceConfig.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/config/PlaceConfig.java
@@ -1,7 +1,15 @@
 package br.com.enzolfranco.place_api.config;
 
+import br.com.enzolfranco.place_api.domain.PlaceRepository;
+import br.com.enzolfranco.place_api.domain.PlaceService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PlaceConfig {
+
+    @Bean
+    PlaceService placeService(PlaceRepository placeRepository){
+        return new PlaceService(placeRepository);
+    }
 }
