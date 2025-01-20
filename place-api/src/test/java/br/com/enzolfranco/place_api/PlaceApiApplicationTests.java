f1bd7a76d422282c7ba6cf3bc64f5b47e020c122
EnzoLFranco <fenzolemos@gmail.com>
2025-01-20 15:50:34 -0300

    feat: Cria o teste de sucess para o create()

--- place-api/src/test/java/br/com/enzolfranco/place_api/PlaceApiApplicationTests.java
+++ place-api/src/test/java/br/com/enzolfranco/place_api/PlaceApiApplicationTests.java
@@ -1,6 +1,7 @@
 package br.com.enzolfranco.place_api;
 
-
+import br.com.enzolfranco.place_api.api.PlaceRequest;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -9,5 +10,25 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 class PlaceApiApplicationTests {
 	@Autowired
 	WebTestClient webTestClient;
+
+	@Test
+	public void testCreatePlaceSucess(){
+		var name = "New Name";
+		var state = "State";
+		var slug = "new-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+	}
 
 }
