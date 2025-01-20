cd2b2d8b0d4be97b7a017cc1c374b42370efdec5
EnzoLFranco <fenzolemos@gmail.com>
2025-01-20 14:45:20 -0300

    feat: Modifica o create() no PlaceService para utilizar o slugify

--- place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceService.java
@@ -1,17 +1,20 @@
 package br.com.enzolfranco.place_api.domain;
 
 import br.com.enzolfranco.place_api.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    public Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(),
                 placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
