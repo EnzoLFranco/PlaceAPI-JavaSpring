6393ba0d91c0d7febbd6980cc756f3e250bab032
EnzoLFranco <fenzolemos@gmail.com>
2025-01-20 15:49:45 -0300

    feat: Adiciona o webTestClient e o Autowired para o mesmo

--- place-api/src/test/java/br/com/enzolfranco/place_api/PlaceApiApplicationTests.java
+++ place-api/src/test/java/br/com/enzolfranco/place_api/PlaceApiApplicationTests.java
@@ -1,13 +1,13 @@
 package br.com.enzolfranco.place_api;
 
-import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	WebTestClient webTestClient;
 
 }
