9a29b134e93f6901e0ae3a8255a0c8424dee2a85
EnzoLFranco <fenzolemos@gmail.com>
2025-01-20 14:36:32 -0300

    fix: Habilita a auditoria R2DBC na PlaceConfig

--- place-api/src/main/java/br/com/enzolfranco/place_api/config/PlaceConfig.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/config/PlaceConfig.java
@@ -4,8 +4,10 @@ import br.com.enzolfranco.place_api.domain.PlaceRepository;
 import br.com.enzolfranco.place_api.domain.PlaceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
