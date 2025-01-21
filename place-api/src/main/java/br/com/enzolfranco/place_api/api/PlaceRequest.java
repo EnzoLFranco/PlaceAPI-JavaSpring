e0426cfa28e09903d70ccca52b1e33075c2ff1b1
EnzoLFranco <fenzolemos@gmail.com>
2025-01-21 15:20:35 -0300

    fix: Remove um import desnecessário do PlaceRequest

--- place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceRequest.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceRequest.java
@@ -2,7 +2,6 @@ package br.com.enzolfranco.place_api.api;
 
 import jakarta.validation.constraints.NotBlank;
 
-import java.time.LocalDateTime;
 
 public record PlaceRequest(
         @NotBlank String name,
