f52de6189b979126b24b55e79d65c5fa646461fc
EnzoLFranco <fenzolemos@gmail.com>
2025-01-21 13:57:36 -0300

    feat: Adiciona a função list() ao controller, instanciando o PlaceService

--- place-api/src/main/java/br/com/enzolfranco/place_api/web/PlaceController.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/web/PlaceController.java
@@ -7,10 +7,8 @@ import br.com.enzolfranco.place_api.domain.PlaceService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -26,6 +24,11 @@ public class PlaceController {
     public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request) {
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
+    }
+
+    @GetMapping
+    public Flux<Place> list(){
+        return placeService.list();
     }
 
 }
