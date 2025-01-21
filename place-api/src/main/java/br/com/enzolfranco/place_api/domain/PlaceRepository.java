c3e8a98f88d686781b34734e9594f7b31d63698a
EnzoLFranco <fenzolemos@gmail.com>
2025-01-21 15:21:04 -0300

    chore: Adiciona a annotation @Repository ao PlaceRepository

--- place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceRepository.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/domain/PlaceRepository.java
@@ -1,6 +1,8 @@
 package br.com.enzolfranco.place_api.domain;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaceRepository extends ReactiveCrudRepository<Place, Long> {
 }
