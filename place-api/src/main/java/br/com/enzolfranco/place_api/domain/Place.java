d8519d9df9fbd3db609ede72d469230e56f96773
EnzoLFranco <fenzolemos@gmail.com>
2025-01-21 14:54:00 -0300

    fix: Adiciona a annotation de @Id à variável id do Place

--- place-api/src/main/java/br/com/enzolfranco/place_api/domain/Place.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/domain/Place.java
@@ -1,11 +1,13 @@
 package br.com.enzolfranco.place_api.domain;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 public record Place(
+        @Id
         Long id,
         String name,
         String slug,
