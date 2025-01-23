1563254d0193152ed165f975f285ad2cea70b930
EnzoLFranco <fenzolemos@gmail.com>
2025-01-23 17:06:00 -0300

    feat: Adiciona as annotations para melhor documentação do schema PlaceResponse

--- place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceResponse.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceResponse.java
@@ -1,8 +1,20 @@
 package br.com.enzolfranco.place_api.api;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
+@Schema(description = "Response object representing a place")
 public record PlaceResponse(
-        String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt
+        @Schema(description = "The name of the place", example = "São Paulo")
+        String name,
+        @Schema(description = "An abbreviation of the place's name to the URL", example = "sao-paulo")
+        String slug,
+        @Schema(description = "The state where the place is located", example = "SP")
+        String state,
+        @Schema(description = "The datetime that the place was created")
+        LocalDateTime createdAt,
+        @Schema(description = "The last datetime that the place was updated")
+        LocalDateTime updatedAt
 ) {
 }
