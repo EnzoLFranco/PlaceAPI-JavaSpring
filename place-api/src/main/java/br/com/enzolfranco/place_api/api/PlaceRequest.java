4d2fd2a8f22120cd2d940e557cab4b6bdfb1727b
EnzoLFranco <fenzolemos@gmail.com>
2025-01-23 17:05:53 -0300

    feat: Adiciona as annotations para melhor documentação do schema PlaceRequest

--- place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceRequest.java
+++ place-api/src/main/java/br/com/enzolfranco/place_api/api/PlaceRequest.java
@@ -1,10 +1,15 @@
 package br.com.enzolfranco.place_api.api;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-
+@Schema(description = "Request object for creating or updating a place")
 public record PlaceRequest(
-        @NotBlank String name,
-        @NotBlank String state
+        @Schema(description = "The name of the place", example = "São Paulo", required = true)
+        @NotBlank
+        String name,
+        @Schema(description = "The state where the place is located", example = "SP", required = true)
+        @NotBlank
+        String state
 ) {
 }
